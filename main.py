cfc51254cb6dbad2b4c3b2e8609c502952d6d5a8
jpqateste <127210494+jpqateste@users.noreply.github.com>
2023-10-10 23:56:28 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,15 @@
+#Atividades da aula de automação 10/10
+
+# Solicitar ao usuário que insira 3 números
+
+n1 = float(input(3.0))
+n2 = float(input(5.0))
+n3 = float(input(7.0))
+
+
+
+media = (n1 + n2 + n3)/3
+
+print(media)
+
+
